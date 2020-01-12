cd74c20edd3d2476e8bb9231e36394299778f5ce
Chibikuri <ryosuke.satoh.wk@gmail.com>
2020-01-13 00:52:30 +0900

    jupyter

---
+++ QuantumInternet/.ipynb_checkpoints/Measurement-based Distributed Quantum Computing-checkpoint.py
@@ -0,0 +1,24 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# # Authors and Title
+# Danos, Vincent, et al. "Distributed measurement-based quantum computation." Electronic Notes in Theoretical Computer Science 170 (2007): 73-94.
+
+# # Contents
+
+# ## What is key idea?
+
+# ## Conclusions and Open Questions
+
+
--- QuantumInternet/Measurement-based Distributed Quantum Computation.md
+++
@@ -1,8 +0,0 @@
-
-# Authors and title
-
-Danos, Vincent, et al. "Distributed measurement-based quantum computation." Electronic Notes in Theoretical Computer Science 170 (2007): 73-94.
-
-## Key idea
-
-## Conclusion and open questions
